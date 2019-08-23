e3883dc20a00c466929afa6b99b73c1a2c892fa3
无可救药 <shuaigeliumingtai@gmail.com>
2019-08-23 14:34:50 +0800

    第一次commit

--- src/main/java/com/liumingtai/mongodemo/controller/HelloController.java
+++ src/main/java/com/liumingtai/mongodemo/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.liumingtai.mongodemo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 public class HelloController {
     @RequestMapping("/hello-world")
